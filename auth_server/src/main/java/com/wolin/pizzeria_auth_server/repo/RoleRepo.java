1883b4735c6880ae52b9a1d15881cde982f8b6ec
Wiktor <w0lin@onet.pl>
2023-01-12 16:09:40 +0100

    Initialization of the project.

--- auth_server/src/main/java/com/wolin/pizzeria_auth_server/repo/RoleRepo.java
+++ auth_server/src/main/java/com/wolin/pizzeria_auth_server/repo/RoleRepo.java
@@ -4,4 +4,5 @@ import com.wolin.pizzeria_auth_server.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoleRepo extends JpaRepository<Role, Integer> {
+    Role findByName(String name);
 }
